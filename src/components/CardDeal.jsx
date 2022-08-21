819cf199bd6023f40c82c6ee2e22239f81770505
mwiechowski <83728694+mwiechowski@users.noreply.github.com>
2022-08-21 22:02:08 +0200

    added more components

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { card } from "../assets"
+import styles, { layout } from "../style"
+import Button from "./Button"
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in a few easy steps</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <Button styles="mt-10" text="Get Started" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-full h-full"/>
+    </div>
+  </section>
+)
 
 export default CardDeal
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { feedback } from "../constants"
+import styles from "../style"
+import FeedbackCard from "./FeedbackCard"
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    {/* gradient TO DO*/}
+    <div />
+    
+    <div className="w-full flex justify-between items-center md:flex-row flex-col mb-16 bg-6 relative z-1">
+      <h1 className={styles.heading2}>What people are <br className="sm:block hidden" /> saying about us</h1>
+      <div>
+        {/* TO DO */}
+      </div>
+    </div>
+  </section>
+)
 
 export default Testimonials
