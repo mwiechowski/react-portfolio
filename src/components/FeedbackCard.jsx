f632bb3c2040b8ef8273977b4c76d094347bf8ce
mwiechowski <83728694+mwiechowski@users.noreply.github.com>
2022-08-22 20:43:19 +0200

    feedback pt. 1

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+const FeedbackCard = ({ key, content, name, title, img }) => (
+  <div className="text-whitesmoke">
+    <div>{key}</div>
+    <div>{content}</div>
+    <div>{name}</div>
+    <div>{title}</div>
+    <div>{img}</div>
+  </div>
+  
+)
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
-  )
-}
 
 export default FeedbackCard
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,10 +8,18 @@ const Testimonials = () => (
     <div />
     
     <div className="w-full flex justify-between items-center md:flex-row flex-col mb-16 bg-6 relative z-1">
-      <h1 className={styles.heading2}>What people are <br className="sm:block hidden" /> saying about us</h1>
-      <div>
-        {/* TO DO */}
+      <h1 className={styles.heading2}>What people are <br className="sm:block hidden" /> saying about us...</h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to become a client and grow your business anywhere on the planet.
+        </p>
       </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-1">
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
     </div>
   </section>
 )
