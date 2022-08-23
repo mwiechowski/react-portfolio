811b622bedf029e98262061876c8c92f1d10c7e5
mwiechowski <83728694+mwiechowski@users.noreply.github.com>
2022-08-23 21:28:33 +0200

    added CTA section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,11 +2,11 @@ import styles from "../style"
 import { clients } from "../constants"
 
 const Clients = () => (
-  <section id="clients" className={`${styles.flexCenter} my-4`}>
+  <section id="collaborations" className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client, index) => (
         <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] mb-5`}>
-          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain companyLogo" />
         </div>
       ))}
     </div>
